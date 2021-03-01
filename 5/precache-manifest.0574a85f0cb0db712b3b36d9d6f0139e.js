38f6d263bbc0949a91938bf32a0d758326287ad7
Tyler Wolf <tylerwolf@stoltm8011.lan>
2021-03-01 22:28:02 +0100

    added 5

---
+++ 5/precache-manifest.0574a85f0cb0db712b3b36d9d6f0139e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba8996a98c2626e967489910569b6dae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afb4833200cf610b3443",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1dd0465cc31076a2ccff",
+    "url": "/static/js/2.15355ea5.chunk.js"
+  },
+  {
+    "revision": "3e54b583583f001f171d219930dc6b65",
+    "url": "/static/js/2.15355ea5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "afb4833200cf610b3443",
+    "url": "/static/js/main.001ba257.chunk.js"
+  },
+  {
+    "revision": "d9aa91fe51545b87aca8",
+    "url": "/static/js/runtime-main.8be218d6.js"
+  }
+]);
